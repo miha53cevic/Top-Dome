ed5550882136fb0fc38f1e9a86ff418f00f4e0cc
miha53cevic <44951515+miha53cevic@users.noreply.github.com>
2018-12-30 13:35:50 +0100

    project files backup

---
+++ TopDome Alpha 1_1 project files/Spawner.h
@@ -0,0 +1,37 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <vector>
+
+#include "Enemy.h"
+#include "normEnemy.h"
+#include "quickEnemy.h"
+#include "normEnemy.h"
+
+#include "Player.h"
+
+#include "Bullet.h"
+
+class Spawner
+{
+public:
+	Spawner();
+	~Spawner() = default;
+
+	void Update(std::string& map, const sf::Vector2i& mapSize, const sf::Vector2i& pixelSize, sf::Time Elapsed, int& Score, std::vector<Bullet>& vecBullets, Player* m_player);
+	void Draw(sf::RenderWindow* window);
+
+	void addNormEnemy(float x, float y);
+	void addQuickEnemy(float x, float y);
+	void addTankEnemy(float x, float y);
+
+	void Reset();
+
+	int getNumberOfEnemies();
+
+private:
+	std::vector<Enemy*> m_vecEnemys;
+
+	int m_NumberOfEnemies;
+	bool m_bWaveOver;
+};
+
---
+++ TopDome Alpha 1_1 project files/window.h
@@ -0,0 +1,38 @@
+#pragma once
+#include <string>
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+
+class Window
+{
+public:
+
+	//Constructors
+	Window();
+	Window(const sf::Vector2u& l_size, const std::string& l_name);
+	~Window();
+
+	//User Called Functions
+	void BeginDraw(sf::Color l_color);
+	void EndDraw();
+
+	void Draw(sf::Drawable& l_drawable);
+
+	void Update(bool& Jump, bool& Shoot, bool& Pause);
+
+	sf::Vector2u getSize();
+	sf::RenderWindow* getWindow();
+
+	bool IsDone();
+
+	void changeIsDone(bool done);
+	
+private:
+	sf::RenderWindow m_window;
+
+	bool bIsDone;
+
+	//Helper Functions
+	void Setup(const sf::Vector2u& l_size, const std::string& l_name);
+	void Destroy();
+};
